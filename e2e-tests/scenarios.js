c259f9849bed4a1a9eebfad1b82f0bae626daf13
kisscool62 <kisscool@gmail.com>
2016-03-11 01:20:00 +0100

    scenario test js

--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -10,7 +10,7 @@ describe('my app', function() {
     expect(browser.getLocationAbsUrl()).toMatch("/view1");
   });
 
-
+/*
   describe('view1', function() {
 
     beforeEach(function() {
@@ -24,19 +24,6 @@ describe('my app', function() {
     });
 
   });
+*/
 
-
-  describe('view2', function() {
-
-    beforeEach(function() {
-      browser.get('index.html#/view2');
-    });
-
-
-    it('should render view2 when user navigates to /view2', function() {
-      expect(element.all(by.css('[ng-view] p')).first().getText()).
-        toMatch(/partial for view 2/);
-    });
-
-  });
 });
